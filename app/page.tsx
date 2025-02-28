d384f57ddec8589a9b02712019ebe6bdc71563c1
Negin <negin.khademi@gmail.com>
2025-02-28 13:40:26 +0330

    setup components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        click me
+      </Button>
+    </div>
+  );
 }
