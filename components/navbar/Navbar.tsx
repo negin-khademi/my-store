e35365475699f1793b5656f1d237970b0475bbb3
Negin <negin.khademi@gmail.com>
2025-02-28 21:37:50 +0330

    Navbar Component - Blueprint

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,4 +1,23 @@
+import CartButton from './CartButton';
+import Container from '../global/Container';
+import DarkMode from './DarkMode';
+import LinkDropdown from './LinkDropdown';
+import Logo from './Logo';
+import NavSearch from './NavSearch';
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="border-b">
+      <Container className="flex gap-4 flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8">
+        <Logo />
+        <NavSearch />
+        <div className="flex gap-4 items-center">
+          <CartButton />
+          <DarkMode />
+          <LinkDropdown />
+        </div>
+      </Container>
+    </nav>
+  );
 }
 export default Navbar;
