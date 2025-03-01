1a3f62508c108bebd082def70d308ea4382e6962
Negin <negin.khademi@gmail.com>
2025-03-01 13:49:45 +0330

    cart Button component

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,4 +1,19 @@
-function CartButton() {
-  return <div>CartButton</div>;
+import { Button } from '../ui/button';
+import Link from 'next/link';
+import { LuShoppingCart } from 'react-icons/lu';
+
+async function CartButton() {
+  const numItemsCart = 9;
+
+  return (
+    <Button asChild variant="outline" size="icon" className="flex justify-center items-center relative">
+      <Link href="/cart">
+        <LuShoppingCart />
+        <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
+          {numItemsCart}
+        </span>
+      </Link>
+    </Button>
+  );
 }
 export default CartButton;
